2e48c6dd5e24e39b62df2f4aa7c3a15b3d21b412
AlexandrFedorow <75899441+AlexandrFedorow@users.noreply.github.com>
2020-12-13 16:37:49 +0300

    дописал часть коментов

--- jpg_convert.py
+++ jpg_convert.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import config
-
+''' тут происходит конвертация в jpg '''
 def convert():
     im = Image.open(config.PNAME)
     bg = Image.new("RGB", im.size, (255, 255,255))
