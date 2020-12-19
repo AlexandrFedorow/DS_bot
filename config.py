193fee19a882f1c0ae57a6b82caa0cfc80e8cb1e
alexandrfedorov <alexandr.fedoroff@icloud.com>
2020-12-19 20:13:36 +0300

    убрал токен

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = ''
+TOKEN = 'токен'
 PNAME = 'img/'
 PICTURE_CGANGE = ['bot обработай пикчу']
 OVERLAY_PHOTO = ['bot наложи смайлик на лицо']
--- te.py
+++ te.py
@@ -1,8 +1,31 @@
+import cv2
 from PIL import Image
 
-watermark= Image.open('img/FOX.png')
-img = Image.open('img/MASK1.png')
+image_path = "img/r.jpg"
+im = Image.open('img/r.jpg')
+watermark = Image.open('img/ch.png')
+watermark = watermark.resize((100, 100), Image.ANTIALIAS)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+image = cv2.imread(image_path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor= 1.1,
+    minNeighbors= 5,
+    minSize=(10, 10)
+)
+
+faces_detected = "Лиц обнаружено: " + format(len(faces))
+print(faces_detected)
+# Рисуем квадраты вокруг лиц
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+    watermark = watermark.resize((w, h), Image.ANTIALIAS)
+    im.paste(watermark, (x, y), watermark)
 
 
-img.paste(watermark, (1000, 1000),  watermark)
-img.save("img/img.png")
+im.show()
+
